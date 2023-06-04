477ec578ab57ff16e3b18c7158da3f7764a2794c
erdogank <112961176+erdogank@users.noreply.github.com>
2023-06-04 18:34:59 +0300

    driverset

--- src/test/java/com/coinmarket/utilities/Driver.java
+++ src/test/java/com/coinmarket/utilities/Driver.java
@@ -57,7 +57,7 @@ public class Driver {
                     break;
 
                 case "chrome-headless":
-                    //System.setProperty("webdriver.chrome.driver","/usr/bin/google-chrome");
+                    System.setProperty("webdriver.chrome.driver","drivers/chromedriver");
 
                     ChromeOptions options = new ChromeOptions();
                     options.addArguments("--headless");
