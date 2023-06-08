df3708dd70a5025febeb3162f5b2e4c5c9a452c9
erdogank <112961176+erdogank@users.noreply.github.com>
2023-06-08 17:30:07 +0300

    with grid

--- src/test/java/com/coinmarket/utilities/Driver.java
+++ src/test/java/com/coinmarket/utilities/Driver.java
@@ -47,7 +47,7 @@ public class Driver {
                 case "remote-chrome":
                     try {
                         // assign your grid server address
-                        String gridAddress = "44.201.88.236";
+                        String gridAddress = "18.212.10.44";
                         URL url = new URL("http://" + gridAddress + ":4444/wd/hub");
 
                         ChromeOptions chromeOptions = new ChromeOptions();
